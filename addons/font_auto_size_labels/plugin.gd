3690a31c2eec398c3b4aae1b4cdee6e8259d4e8a
Lescandez <109640033+LuisEscorza@users.noreply.github.com>
2025-07-20 11:51:22 -0400

    Fix custom type script path
    Corrected the script path for 'AutoSizeRichTextlabel' from 'auto_size_rich_label.gd' to 'auto_size_rich_text_label.gd' to ensure proper loading in the plugin.

--- addons/font_auto_size_labels/plugin.gd
+++ addons/font_auto_size_labels/plugin.gd
@@ -5,7 +5,7 @@ extends EditorPlugin
 #region Virtual functions
 func _enter_tree():
 	add_custom_type("AutoSizeLabel", "Label", preload("auto_size_label.gd"), preload("icon.svg"))
-	add_custom_type("AutoSizeRichTextlabel", "RichTextLabel", preload("auto_size_rich_label.gd"), preload("icon.svg"))
+	add_custom_type("AutoSizeRichTextlabel", "RichTextLabel", preload("auto_size_rich_text_label.gd"), preload("icon.svg"))
 
 
 func _exit_tree():
